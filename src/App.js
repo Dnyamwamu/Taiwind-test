5c15b52c964cd3943286c99cd848d8e9edd4d3de
Dnyamwamu <dnyamwamu@gmail.com>
2022-08-29 08:58:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="container bg-cyan-500 w-full h-screen">
+      
+        
+        <h1>Hello world!</h1>
+        <p>Tailwind test</p>
+      
+    </div>
+  );
+}
+
+export default App;
